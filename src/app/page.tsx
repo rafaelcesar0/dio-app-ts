3d489331d4306f7dbd67c59817361c599d9ac7ad
rafaelcesar0 <rafaelcesar988@gmail.com>
2024-01-08 18:47:59 -0300

    style completo 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,64 @@
-import Card from '@/components/Card';
-import Layout from '@/components/Layout';
+'use client';
 
-export default function Home() {
+import Layout from '@/components/Layout';
+import { useState } from 'react';
+
+const Home = () => {
+    const [showPassword, setShowPassword]: [boolean, Function] =
+        useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword: boolean) => !prevShowPassword);
+    };
+
     return (
-        <Layout>
-            Hello World!
-            <Card
-                id={1}
-                paragraph="TypeScript"
-                details="TS para front-end e back-end"
-            />
-            <Card id={2} paragraph="HTML" details="HTML para front-end" />
-            <Card
-                id={3}
-                paragraph="SQL"
-                details="SQL para banco de dados"
-            />
-        </Layout>
+        <main className="font-bold text-2xl h-screen w-screen bg-gradient-to-b from-blue-400 to-blue-700 flex justify-center">
+            <Layout>
+                <h1 className="flex justify-center text-3xl font-extrabold mb-10">
+                    Faça o login
+                </h1>
+                <div className="flex flex-col relative">
+                    <label htmlFor="emailInput" className="font-normal">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="emailInput"
+                        placeholder="joão@gmail.com"
+                        className="pl-2 pr-2 border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                    />
+                </div>
+                <br />
+                <div className="flex flex-col relative">
+                    <label htmlFor="passwordInput" className="font-normal">
+                        Senha
+                    </label>
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        id="passwordInput"
+                        placeholder={
+                            showPassword ? 'Minha senha' : '•••••••••••'
+                        }
+                        className="pl-2 pr-10 border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                    />
+                    <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-600 cursor-pointer mt-8"
+                    >
+                        {showPassword ? '🙉' : '🙈'}
+                    </button>
+                </div>
+                <br />
+                <button
+                    type="submit"
+                    className="bg-blue-400 px-3 py-1 mt-4 rounded flex transition duration-300 mx-auto hover:bg-blue-500 text-lg text-white "
+                >
+                    CONTINUAR
+                </button>
+            </Layout>
+        </main>
     );
-}
+};
+
+export default Home;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      Dio Banck
+    </div>
   )
 }
 
