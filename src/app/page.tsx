0bcd3bba64a263addbcc417fd0b0245413bbe771
rafaelcesar0 <rafaelcesar988@gmail.com>
2024-01-10 21:28:18 -0300

    micro atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,25 +12,25 @@ const Home = () => {
     };
 
     return (
-        <main className="font-bold text-2xl h-screen w-screen bg-gradient-to-b from-blue-400 to-blue-700 flex justify-center">
+        <main className="font-bold h-screen w-screen bg-gradient-to-b from-blue-400 to-blue-700 flex justify-center">
             <Layout>
                 <h1 className="flex justify-center text-3xl font-extrabold mb-10">
                     Faça o login
                 </h1>
                 <div className="flex flex-col relative">
-                    <label htmlFor="emailInput" className="font-normal">
+                    <label htmlFor="emailInput">
                         Email
                     </label>
                     <input
                         type="email"
                         id="emailInput"
                         placeholder="joão@gmail.com"
-                        className="pl-2 pr-2 border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                        className="pl-2 pr-2 font-normal border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
                     />
                 </div>
                 <br />
                 <div className="flex flex-col relative">
-                    <label htmlFor="passwordInput" className="font-normal">
+                    <label htmlFor="passwordInput">
                         Senha
                     </label>
                     <input
@@ -39,12 +39,12 @@ const Home = () => {
                         placeholder={
                             showPassword ? 'Minha senha' : '•••••••••••'
                         }
-                        className="pl-2 pr-10 border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
+                        className="pl-2 pr-10 font-normal border-2 border-gray-300 rounded-md focus:border-blue-500 focus:outline-none"
                     />
                     <button
                         type="button"
                         onClick={togglePasswordVisibility}
-                        className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-600 cursor-pointer mt-8"
+                        className="absolute inset-y-0 right-0 text-xl flex items-center px-2 text-gray-600 cursor-pointer mt-6"
                     >
                         {showPassword ? '🙉' : '🙈'}
                     </button>
